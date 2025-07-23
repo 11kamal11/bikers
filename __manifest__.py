70c4906dcc9a9d07c4db00adcee37e047d07f6a3
11kamal11 <kamal9@emails.emizentech.com>
2025-07-23 17:02:35 +0530

    additioanl changes

--- __manifest__.py
+++ __manifest__.py
@@ -13,14 +13,21 @@
     """,
     'author': 'Kamal',
     'website': 'https://www.yourcompany.com',
-    'depends': ['base', 'website', 'portal'],  
+    'depends': ['base', 'website', 'portal'],
     'data': [
+        'security/models.xml',
         'security/ir.model.access.csv',
         'views/bike_rental_views.xml',
         'views/bike_rental_request_views.xml',
-        'views/website_bike_rental_templates.xml',  
+        'views/website_bike_rental_templates.xml',
         'data/bike_rental_data.xml',
     ],
+    'assets': {
+        'web.assets_frontend': [
+            'bike_rental/static/src/css/bike_rental.css',
+            'bike_rental/static/src/js/bike_rental.js',
+        ],
+    },
     'demo': [],
     'installable': True,
     'application': True,
